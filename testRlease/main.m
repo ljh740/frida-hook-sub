e69b1c8599dfc404e07aa21109735f4c2f17e6f7
罗杰鸿 <luojiehong@darkedge.cn>
2020-05-22 18:44:20 +0800

    Initial Commit

---
+++ testRlease/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  testRlease
+//
+//  Created by jie on 2020/5/22.
+//  Copyright © 2020 jie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
